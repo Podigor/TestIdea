b50a2236dfdb2c6bfd2a1f626f608ea816f6b892
Ihor Podielnikov <Podigor@i.ua>
2023-03-15 20:27:57 +0200

    New commit message

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import com.example.test.NewClass;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Hello IDEA!");
+
+        NewClass item = new NewClass();
+        item.sayHello();
+
+    }
+}
---
+++ src/com/example/test/NewClass.java
@@ -0,0 +1,7 @@
+package com.example.test;
+
+public class NewClass {
+    public void sayHello() {
+        System.out.println("Hello Git!!!");
+    }
+}
